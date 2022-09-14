44aae5845c80cf4dcf4637696300944bb6f294d0
amar_bcool <apple@apples-MacBook-Pro.local>
2022-09-14 08:20:07 +0530

    Firts commit

---
+++ src/test/java/com/amar/customer/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amar.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
