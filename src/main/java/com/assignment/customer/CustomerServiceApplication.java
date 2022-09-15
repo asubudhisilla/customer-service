41347bcfb41b7cb5c0898caef304b2a8337cae25
asubudhisilla <apple@apples-MacBook-Pro.local>
2022-09-15 23:26:23 +0530

    swagger-document

--- src/main/java/com/assignment/customer/CustomerServiceApplication.java
+++ src/main/java/com/assignment/customer/CustomerServiceApplication.java
@@ -2,6 +2,10 @@ package com.assignment.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableWebMvc
 @SpringBootApplication
 public class CustomerServiceApplication {
 	public static void main(String[] args) {
--- src/main/java/com/assignment/customer/configuration/CustomerConfiguration.java
+++ src/main/java/com/assignment/customer/configuration/CustomerConfiguration.java
@@ -3,11 +3,17 @@ package com.assignment.customer.configuration;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 public class CustomerConfiguration {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public InternalResourceViewResolver defaultViewResolver() {
+        return new InternalResourceViewResolver();
     }
 }
