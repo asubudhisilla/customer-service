95f7ab080253bae22bf1915b9526d2d34f48893c
asubudhisilla <apple@apples-MacBook-Pro.local>
2022-09-16 09:50:32 +0530

    buildfix

--- src/test/java/com/assignment/customer/integration/CustomerControllerIntegrationTest.java
+++ src/test/java/com/assignment/customer/integration/CustomerControllerIntegrationTest.java
@@ -5,7 +5,6 @@ import com.assignment.customer.CustomerServiceApplication;
 import com.assignment.customer.configuration.DataSourceConfig;
 import com.assignment.customer.configuration.TestDataSourceConfig;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -19,9 +18,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -30,7 +26,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest(classes = CustomerServiceApplication.class,
         webEnvironment = WebEnvironment.RANDOM_PORT)
-@Import({DataSourceConfig.class, TestDataSourceConfig.class})
 @Testcontainers
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
